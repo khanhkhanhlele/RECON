336bd467f4e925193e11ee5e84ee49f52f1f3c74
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-05-18 13:55:35 +0700

    .

--- scripts/train_mnist.py
+++ scripts/train_mnist.py
@@ -13,7 +13,7 @@ import torch.optim as optim
 import torch.nn as nn
 
 import sys
-sys.path.extend(['../'])
+sys.path.extend(['../../'])
 from utils.common import make_exp_and_log_dirs, set_random_seed
 
 from utils.logger import get_root_logger, init_wandb_logger, MessageLogger, \
