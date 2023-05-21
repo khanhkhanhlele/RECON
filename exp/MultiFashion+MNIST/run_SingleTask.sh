75f3a5781059583a57ed0dc00187ff9a854c9091
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-05-21 21:54:13 +0700

    Revert "update data"
    This reverts commit 1194eb99f9ea5bdbd4beebc702f0410a33c3d3e4.

--- exp/MultiFashion+MNIST/run_SingleTask.sh
+++ exp/MultiFashion+MNIST/run_SingleTask.sh
@@ -5,7 +5,7 @@
 #
 #CUDA_VISIBLE_DEVICES=1 python ./scripts/train_mnist.py --name $name --method $method --seed $seed --task $task
 
-
+#python train_mnist.py --name SingleTask_T1 --method 'Baseline' --seed 0 --task 'T1'
 seed=0
 name=SingleTask_T2
 task='T2'
